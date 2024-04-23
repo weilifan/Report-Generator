52c1fe1a230378c0afe92b3e67a5f3a4e606e283
weilifan <liyixin24@gmail.com>
2024-04-23 20:37:22 +0800

    m3e句向量模型

--- embedding_model.py
+++ embedding_model.py
@@ -12,7 +12,7 @@ class EmbModel:
 
 
 if __name__ == '__main__':
-    path = "m3e-base"
+    path = "D:/BaiduNetdiskDownload/Code/moka-ai_m3e-base"
     content = "2006年，Geoffrey Hinton提出通过逐层无监督预训练的方式来缓解由于梯度消失而导致的深层网络难以训练的问题，为神经网络的有效学习提供了重要的优化途径。"
     emb_model = EmbModel(path)
     emb = emb_model.to_emb(content)[0]
